d68d5eb2dfb41d615993bbee35870f4d46953cd5
bhavikjogi94 <jogi.net7@gmail.com>
2016-11-10 17:23:56 -0500

    Api

---
+++ BhavikAPI/Controllers/BhavikAPIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BhavikAPI.Controllers
+{
+    public class BhavikAPIController : ApiController
+    {
+        private Bhavik_APIEntities context = new Bhavik_APIEntities();
+        [HttpGet]
+        public IEnumerable<employee> Getdata()
+        {
+            IEnumerable<employee> countries = context.employees;
+            if (countries == null)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No record(s) found."));
+            }
+            else
+            {     //List<employee> simple = 
+                return countries;
+            }
+        }
+    }
+}
---
+++ BhavikAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BhavikAPI.WebApiApplication" Language="C#" %>
